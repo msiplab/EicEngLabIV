f892c1c7bcf955ffc237cda074a633d78eb419bb
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2020-06-27 14:17:24 +0900

    ガウス基底関数の単変量への対応を実装

--- gauss_kmeans.py
+++ gauss_kmeans.py
@@ -10,7 +10,10 @@
 
 Copyright (c) 2020, Shogo MURAMATSU, All rights reserved.
 """
+import sys
 import numpy as np
+import pandas as pd 
+from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -32,13 +35,13 @@ class GaussianFeaturesWithKmeans(BaseEstimator, TransformerMixin):
 
     """
 
-    def __init__(self, nbfs, width_factor=1.0, Xobs=None):
+    def __init__(self, nbfs, width_factor=1.0, prekmeans=True):
         """GaussianFeaturesWithKMeansのコンストラクタ
 
         Args:
             nbfs (int): 基底数
             width_factor (float): 基底関数の広がり係数
-            Xobs (np.ndarray): 引数の説明
+            prekmeans (bool): K-平均法により前処理
 
         Returns: 
            GaussianFeaturesWithKMeans: ガウス基底関数オブジェクト
@@ -60,21 +63,54 @@ class GaussianFeaturesWithKmeans(BaseEstimator, TransformerMixin):
         """
         self.nbfs = nbfs
         self.width_factor = width_factor
+        self.prekmeans = prekmeans
 
-        self.Xobs = Xobs
-    
     @staticmethod
     def _gauss_basis(x, y, width, axis=None):
-        arg = (x - y) / width
+        if any(width==0):
+            raise ValueError('基底関数が多すぎるようです')
+        else:
+            arg = (x - y) / width
         return np.exp(-0.5 * np.sum(arg ** 2, axis))
 
     def fit(self, X, y=None):
-        # create N centers spread along the data range
-        self.centers_ = np.linspace(X.min(), X.max(), self.nbfs)
-        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        # print('fit')        
+        if self.prekmeans:
+            kmeans = KMeans(n_clusters=self.nbfs,random_state=0)
+            kmeans.fit(X)
+            labels = kmeans.predict(X).reshape(-1,1)
+            clusters = pd.DataFrame(np.concatenate((labels,X),axis=1)).groupby([0])
+            self.centers_ = kmeans.cluster_centers_.reshape(-1,)
+            self.widths_ = self.width_factor * clusters.std(ddof=0).to_numpy().reshape(-1,)
+        else:
+            self.centers_ = np.linspace(X.min(), X.max(), self.nbfs)
+            self.widths_ = self.width_factor * (self.centers_[1] - self.centers_[0]) * np.ones(self.nbfs)
         return self
 
     def transform(self, X):
+        # print('transform')
         return self._gauss_basis(X[:,:, np.newaxis], self.centers_,
-                                self.width_, axis=1)
+                                self.widths_, axis=1)
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt 
+    import seaborn as sns; sns.set()
+    
+    M = 10
+    nSamples = 50
+    rng = np.random.RandomState(1)
+    x = 10 * rng.rand(nSamples)
+    y = np.sin(x) + 0.1 * rng.randn(nSamples)
+
+    phi = GaussianFeaturesWithKmeans(M,width_factor=1.0,prekmeans=True)
+    gauss_model = make_pipeline(phi,LinearRegression())
+    gauss_model.fit(x[:, np.newaxis], y)
+    xfit = np.linspace(0, 10, 1000)
+    yfit = gauss_model.predict(xfit[:,np.newaxis])
+    
+    fig, ax = plt.subplots()
+    ax.scatter(x,y)
+    ax.plot(xfit,yfit)
+    ax.set_xlim(0,10)
+    plt.show()
     
--- gauss_kmeans_testcase.py
+++ gauss_kmeans_testcase.py
@@ -12,6 +12,8 @@
 """
 import unittest
 import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
 from numpy.testing import assert_array_equal
 from gauss_kmeans import GaussianFeaturesWithKmeans
 
@@ -21,7 +23,7 @@ class GaussianFeaturesWithKMeansTestCase(unittest.TestCase):
      GaussianFeaturesWithKMeansクラスのユニットテストを定義
 
     """
-    
+
     def test_construction(self):
         """コンストラクタのテスト"""
 
@@ -34,50 +36,111 @@ class GaussianFeaturesWithKMeansTestCase(unittest.TestCase):
         # 期待値
         expctd_nbfs = M
         expctd_width_factor = 1.0
-        expctd_Xobs = None
-
+        expctd_prekmeans = True
+        
         # 実現値
         actual_nbfs = phi.nbfs
         actual_width_factor = phi.width_factor
-        actual_Xobs = phi.Xobs
-
+        actual_prekmeans = phi.prekmeans
 
         # 評価
         self.assertEqual(actual_nbfs,expctd_nbfs)
         self.assertEqual(actual_width_factor,expctd_width_factor)
-        self.assertEqual(actual_Xobs,expctd_Xobs)
+        self.assertEqual(actual_prekmeans,expctd_prekmeans)
 
-    def test_construction_Xobs1d(self):
+    def test_x1d_wo_kmeans(self):
+        """テスト"""
 
         # 設定
         M = 10
-        nSamples = 100
+        nSamples = 50
         rng = np.random.RandomState(1)
-        Xobs = rng.rand(nSamples)
+        x = 10 * rng.rand(nSamples)
+        X = x.reshape(-1,1)
 
         # ターゲットのインスタンス化
-        phi = GaussianFeaturesWithKmeans(nbfs=M,Xobs=Xobs)
+        phi = GaussianFeaturesWithKmeans(nbfs=M,prekmeans=False)
+        phi.fit(X)
+
+        #rom sklearn.linear_model import LinearRegression 
+        #from sklearn.pipeline import make_pipeline
+        #mymodel = make_pipeline(phi,LinearRegression())
+        #mymodel.fit(x[:,np.newaxis],y)
+        #xfit = np.linspace(0, 10, 1000)
+        #yfit = mymodel.predict(xfit[:,np.newaxis])
 
         # 期待値
         expctd_nbfs = M
         expctd_width_factor = 1.0
-        expctd_Xobs = Xobs
-    
+        expctd_prekmeans = False
+        expctd_centers = np.linspace(X.min(), X.max(), M)
+        expctd_widths = (expctd_centers[1] - expctd_centers[0])*np.ones(M)
+        
         # 実現値
         actual_nbfs = phi.nbfs
         actual_width_factor = phi.width_factor
-        actual_Xobs = phi.Xobs
+        actual_prekmeans = phi.prekmeans
+        actual_centers = phi.centers_
+        actual_widths = phi.widths_
 
         # 評価
         self.assertEqual(actual_nbfs,expctd_nbfs)
         self.assertEqual(actual_width_factor,expctd_width_factor)
-        assert_array_equal(actual_Xobs,expctd_Xobs)
+        self.assertEqual(actual_prekmeans,expctd_prekmeans)
+        assert_array_equal(actual_widths,expctd_widths)
+        assert_array_equal(actual_centers,expctd_centers)
 
+    def test_x1d_w_kmeans(self):
+        """テスト"""
 
+        # 設定
+        M = 10
+        rng = np.random.RandomState(1)
+        x = 10 * rng.rand(50)
+        y = np.sin(x) + 0.1 * rng.randn(50)
+        X = x.reshape(-1,1)
+        kmeans = KMeans(n_clusters=M,random_state=0)
+        kmeans.fit(X)
 
+        # ターゲットのインスタンス化
+        phi = GaussianFeaturesWithKmeans(nbfs=M,prekmeans=True)
+        phi.fit(X)
 
+        #rom sklearn.linear_model import LinearRegression 
+        #from sklearn.pipeline import make_pipeline
+        #mymodel = make_pipeline(phi,LinearRegression())
+        #mymodel.fit(x[:,np.newaxis],y)
+        #xfit = np.linspace(0, 10, 1000)
+        #yfit = mymodel.predict(xfit[:,np.newaxis])
+
+        # 期待値
+        expctd_nbfs = M
+        expctd_width_factor = 1.0
+        expctd_prekmeans = True
+        expctd_centers = kmeans.cluster_centers_.reshape(-1,)
+        labels = kmeans.predict(X).reshape(-1,1)
+        clusters = pd.DataFrame(np.concatenate((labels,X),axis=1)).groupby([0])
+        expctd_widths = clusters.std(ddof=0).to_numpy().reshape(-1,)
+    
+        #import matplotlib.pyplot as plt 
+        #import seaborn as sns; sns.set()
+        #fig, ax = plt.subplots()
+        #ax.scatter(X,y,c=c)
+        #plt.show()
         
-        
+        # 実現値
+        actual_nbfs = phi.nbfs
+        actual_width_factor = phi.width_factor
+        actual_prekmeans = phi.prekmeans
+        actual_centers = phi.centers_
+        actual_widths = phi.widths_
+
+        # 評価
+        self.assertEqual(actual_nbfs,expctd_nbfs)
+        self.assertEqual(actual_width_factor,expctd_width_factor)
+        self.assertEqual(actual_prekmeans,expctd_prekmeans)
+        assert_array_equal(actual_widths,expctd_widths,'widths')
+        assert_array_equal(actual_centers,expctd_centers,'centers')
 
 if __name__ == '__main__':
     unittest.main()
